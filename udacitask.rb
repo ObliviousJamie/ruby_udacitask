0f3acb0e1b5f2e9cebb0609f21642ea9061f2ae7
Jamie Moreland <jamiemorelanduk@gmail.com>
2016-05-31 13:07:10 +0100

    Changed so list is order by priority

--- udacitask.rb
+++ udacitask.rb
@@ -4,39 +4,34 @@ require_relative 'todolist.rb'
 list = TodoList.new("Things to learn")
 
 # Add four new items
-list.add_item("Learn Ruby", 10)
-list.add_item("Learn German", 3)
 list.add_item("Learn Rails")
+list.add_item("Learn Ruby", 10)
 list.add_item("Learn new VIM commands")
-
+list.add_item("Learn German", 3)
 
 # Print the list
-list.print_items
-
-#list.print_by_priority
-
-list.has_urgent?
+list.print_by_priority
 
 # Delete the first item
 list.remove_item(0)
 
-
 # Print the list
-list.print_items
+list.print_by_priority
 
 # Delete the second item
 list.remove_item(1)
 
 # Print the list
-list.print_items
+list.print_by_priority
 
 # Update the completion status of the first item to complete
 list.get_item(0).update_status
 
-
 # Print the list
-list.print_items
+list.print_by_priority
 
 # Update the title of the list
+list.title = "Little list of things to learn"
 
 # Print the list
+list.print_by_priority
