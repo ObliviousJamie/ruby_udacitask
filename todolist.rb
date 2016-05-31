2c7c4556b430bba6e8cbed8712eb1981724df1c7
Jamie Moreland <jamiemorelanduk@gmail.com>
2016-05-31 12:59:03 +0100

    Priority implemented

--- todolist.rb
+++ todolist.rb
@@ -1,28 +1,66 @@
 class TodoList
     # methods and stuff go here
     attr_reader :title, :items
+    attr_writer :title
     # Initialize and tod list with title and no items
     def initialize(list_title)
         @title = list_title
         @items = Array.new
-    end
+    end 
+
+     #Adds an item
+     def add_item(new_item, priority = 1)
+         item = Item.new(new_item, priority)
+         @items.push item
+     end
+
+     def remove_item(index)
+         @items.delete_at(index) 
+     end
+
+     def has_urgent?
+         puts @items.any?{|item| item.priority > 8}
+     end
+
+     def print_items(list = @items)
+         puts "+++++#{@title} +++++ "
+         list.each{|list_item| list_item.print_details}
+         puts ""
+     end
+
+     def print_by_priority
+         sorted = @items.sort{|item_1,item_2| item_2.priority.to_i <=> item_1.priority.to_i}
+         print_items sorted
+     end
+
+     def get_item(index)
+         @items[index]
+     end
 end
 
 class Item
     # methods and stuff go here
-    attr_reader :description, :completed_status
+    attr_reader :description, :completed_status, :priority
 
      # Initialize item with a description and marked as
      # not complete
-     def initialize(item_description)
+     def initialize(item_description, priority = 1)
         @description = item_description
         @completed_status = false
+        @priority = priority 
+     end
+
+     def update_status
+         @completed_status = !@completed_status
+     end
+
+     def print_status
+         puts @completed_status
      end
      
-     #Adds an item
-     def add_item(new_item)
-         item = Item.new(new_item)
-         @items.push(item)
+     def print_details
+         puts "#{description} Completed:#{@completed_status} Priority: #{@priority} " 
      end
+
 
 end
--- udacitask.rb
+++ udacitask.rb
@@ -1,22 +1,41 @@
 require_relative 'todolist.rb'
 
 # Creates a new todo list
+list = TodoList.new("Things to learn")
 
 # Add four new items
+list.add_item("Learn Ruby", 10)
+list.add_item("Learn German", 3)
+list.add_item("Learn Rails")
+list.add_item("Learn new VIM commands")
+
 
 # Print the list
+list.print_items
+
+#list.print_by_priority
+
+list.has_urgent?
 
 # Delete the first item
+list.remove_item(0)
+
 
 # Print the list
+list.print_items
 
 # Delete the second item
+list.remove_item(1)
 
 # Print the list
+list.print_items
 
 # Update the completion status of the first item to complete
+list.get_item(0).update_status
+
 
 # Print the list
+list.print_items
 
 # Update the title of the list
 
